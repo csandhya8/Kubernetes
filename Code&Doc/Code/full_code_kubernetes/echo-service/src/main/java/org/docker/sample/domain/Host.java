4a239a75670d4d80685b3b6514662b33116be31d
sandhya <sandhyasekarnct@gmail.com>
2017-11-13 17:27:54 +0530

    first commit

---
+++ Code&Doc/Code/full_code_kubernetes/echo-service/src/main/java/org/docker/sample/domain/Host.java
@@ -0,0 +1,39 @@
+package org.docker.sample.domain;
+
+
+public class Host {
+
+    private String address;
+    private int port;
+    private String hostName;
+
+    public Host(String address, int port, String hostName) {
+        this.address = address;
+        this.port = port;
+        this.hostName = hostName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public String getHostName() {
+        return hostName;
+    }
+
+    public void setHostName(String hostName) {
+        this.hostName = hostName;
+    }
+}
---
+++ Code&Doc/Code/full_code_kubernetes/echo-service/src/main/java/org/docker/sample/rest/EchoController.java
@@ -0,0 +1,33 @@
+package org.docker.sample.rest;
+
+import org.docker.sample.domain.Host;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@RestController()
+public class EchoController {
+
+    @RequestMapping(value = "/echo", method = RequestMethod.GET, produces = "application/json")
+    public Host getCustomer(HttpServletRequest request) {
+
+
+        return new Host(request.getLocalAddr(), request.getLocalPort(), getHostName());
+
+    }
+
+    private String getHostName() {
+        InetAddress addr;
+        try {
+            addr = InetAddress.getLocalHost();
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
+        }
+        return addr.getHostName();
+    }
+
+}
